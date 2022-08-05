bfd2a145deb7cedf5f9fe5688e2c7de6b1d2fbc5
Jacob Yacovelli <jacob@yacohome.com>
2022-08-05 07:09:51 -0700

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'playwright13-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
